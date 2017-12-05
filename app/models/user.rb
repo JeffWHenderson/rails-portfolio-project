b83a28533d5be875f3fec6b8f459e632a6821389
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-05 17:36:30 -0500

    cleaned up some of the comment code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: %i[facebook]
 
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    #user.name = auth.info.name   # assuming the user model has a name
-    #user.image = auth.info.image # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails,
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 end
-end
+#     This is what facebook sends
+# <OmniAuth::AuthHash credentials=#
+# <OmniAuth::AuthHash expires=true expires_at=1517694882
+#  token="EAACZAVMLDOHkBAGLOiU1RezC0LghQtoMsg54yNNA19R2BuJns8ag8LZBIj73mwDH1mfx7F57NadVd8jRmDInS9IOTWcZBzBaacedHkQy7fXdFOa3NcygitZBSkwYpNROM4C3TrgYRnN5p66dwoKnNU2C3Q7R77aNnywsZBwZByNwZDZD"> extra=#<OmniAuth::AuthHash raw_info=#<OmniAuth::AuthHash
+# email="jhende34@myvu.vinu.edu" id="10155920593441031" name="Jeffrey Henderson">> info=#<
+# OmniAuth::AuthHash::InfoHash
+# email="jhende34@myvu.vinu.edu" image="http://graph.facebook.com/v2.6/10155920593441031/picture" name="Jeffrey Henderson"> provider="facebook" uid="10155920593441031">

6d8f6948f76bf72e5639444a43759e455f170fca
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-06 11:31:05 -0500

    added associations to models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :user_groups
+  has_many :groups, through: :user_groups
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   devise :omniauthable, omniauth_providers: %i[facebook]

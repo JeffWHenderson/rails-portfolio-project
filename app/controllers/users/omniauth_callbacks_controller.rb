c51806eb2bbe35783f9d05ef044331765105f56b
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-05 20:14:09 -0500

    stubbed user info on index page.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,6 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       current_user = @user #### I added this but i think it makes sense.
+      session[:user] = @user
       redirect_to root_path
       # sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
 

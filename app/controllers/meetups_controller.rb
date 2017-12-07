4ddfdb4be148b58fc75f702f1d91441087a0f493
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-07 16:57:05 -0500

    added a new page with new and update flow in the controller.

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -10,9 +10,20 @@ class MeetupsController < ApplicationController
   end
 
   def new
+    @group = Group.find(params[:group_id])
+    @meetup = Meetup.new#find(params[:id])
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @meetup = Meetup.new(meetup_params)
+    if @meetup.save
+      flash[:notice] = "save succuessful"
+      redirect_to group_meetup_path(@group, @meetup)
+    else
+      flash[:notice] = "meetup creation was unsuccessful"
+      redirect_to root_path
+    end
   end
 
   #needs authorizations
@@ -25,12 +36,18 @@ class MeetupsController < ApplicationController
   # could i look at the First instance of a user_group and verify THAT was the creator?
   def destroy
   end
+
+  private
+
+  def meetup_params
+    params.require(:meetup).permit(:name, :location, :day, :time, :group_id)
+  end
 end
-# group_meetups GET   /groups/:group_id/meetups(.:format)          meetups#index
-#      POST           /groups/:group_id/meetups(.:format)          meetups#create
-# new_group_meetup GET    /groups/:group_id/meetups/new(.:format)      meetups#new
-# edit_group_meetup GET   /groups/:group_id/meetups/:id/edit(.:format) meetups#edit
-# group_meetup GET     /groups/:group_id/meetups/:id(.:format)      meetups#show
-#      PATCH         /groups/:group_id/meetups/:id(.:format)      meetups#update
-#      PUT           /groups/:group_id/meetups/:id(.:format)      meetups#update
-#      DELETE        /groups/:group_id/meetups/:id(.:format)      meetups#destroy
+# group_meetups GET       /groups/:group_id/meetups(.:format)           meetups#index
+#      POST               /groups/:group_id/meetups(.:format)           meetups#create
+# new_group_meetup GET    /groups/:group_id/meetups/new(.:format)       meetups#new
+# edit_group_meetup GET   /groups/:group_id/meetups/:id/edit(.:format)  meetups#edit
+# group_meetup GET      /groups/:group_id/meetups/:id(.:format)         meetups#show
+#      PATCH            /groups/:group_id/meetups/:id(.:format)         meetups#update
+#      PUT              /groups/:group_id/meetups/:id(.:format)         meetups#update
+#      DELETE           /groups/:group_id/meetups/:id(.:format)         meetups#destroy

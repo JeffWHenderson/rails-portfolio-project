b4a00e93ea17db721178ecb63f771e371abff09b
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-07 14:04:09 -0500

    added functions to the groups and meetups controllers.

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,28 @@
 class MeetupsController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+
+  #needs authorizations
+  def edit
+  end
+  def update
+  end
+  #how can I verify that only the creator of a meetup can delete it????
+  # could i look at the First instance of a user_group and verify THAT was the creator?
+  def destroy
+  end
 end
+# group_meetups GET   /groups/:group_id/meetups(.:format)          meetups#index
+#      POST           /groups/:group_id/meetups(.:format)          meetups#create
+# new_group_meetup GET    /groups/:group_id/meetups/new(.:format)      meetups#new
+# edit_group_meetup GET   /groups/:group_id/meetups/:id/edit(.:format) meetups#edit
+# group_meetup GET     /groups/:group_id/meetups/:id(.:format)      meetups#show
+#      PATCH         /groups/:group_id/meetups/:id(.:format)      meetups#update
+#      PUT           /groups/:group_id/meetups/:id(.:format)      meetups#update
+#      DELETE        /groups/:group_id/meetups/:id(.:format)      meetups#destroy

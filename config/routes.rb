439fe0ce8a2e51d6d6203f4008a0975d349e2cec
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-07 13:54:19 -0500

    created a nested routes for groups/meetups.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :meetups
-  resources :groups
-  resources :user_groups
+
+  #resources :user_groups
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
 
   root 'users#index'
 
+  resources :groups do
+    resources :meetups
+  end
   #get '/', to: 'user/sessions#create'
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

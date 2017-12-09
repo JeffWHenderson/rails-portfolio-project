10e746b752df3e9d96102e837450c4578eddb49a
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-09 16:47:42 -0500

    gixed a small typo that was keeping my has_many_through from properly associating User Model and Group model.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
-  has_many :user_groups
-  has_many :users, through: :groups
   has_many :meetups
-
-  validates :name, presence: true
+  has_many :user_groups
+  has_many :users, through: :user_groups
+  
+  #validates :name, presence: true
 end

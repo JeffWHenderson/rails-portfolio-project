93fd3bed6daa169dd430334268c3c8e1db7db65e
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-19 17:53:39 -0500

    added logic to custom writer for meetup_tags.

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -4,6 +4,15 @@ class Meetup < ApplicationRecord
   has_many :tags, through: :meetup_tags
 
   def tags_attributes=(tags_hashes)
-    raise tags_hashes.inspect
+    #raise tags_hashes.inspect
+    #{"0"=>{"name"=>"Spooky"}}
+    tags_hashes.each do |i, tags_attributes|
+      if tags_attributes[:name].present?
+        tag = Tag.find_or_create_by(name: tags_attributes[:name])
+        if !self.tags.include?(tag)
+          self.meetup_tags.build(:tag => tag)
+        end
+      end
+    end
   end
 end

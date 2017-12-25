2075024f326b8da5c7fcc11f0cc3c7bae8a40c7c
JeffWHenderson <jeffrey.henderson17@gmail.com>
2017-12-24 21:01:45 -0500

    added strong params to user_groups and cleaned up the comment method.

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,26 +1,12 @@
 class UserGroupsController < ApplicationController
   def comment
-    comment = params['comment'].inspect
-    user_id = params['user_id']
-    group_id = params['group_id']
-    ug = UserGroup.new(:comment => comment, :user_id => user_id, :group_id => group_id)
-    ug.save
-    redirect_to group_path(group_id)
-  end
-
-  def create
-    @group = Group.find(params[:group_id])
-    @user = session[:user]
-    @new_user_group = UserGroup.new(:user_id => session[:user]['id'], :group => @group)
-    if @new_user_group.save
-      flash[:notice] = "Successfully Joined Group"
-      redirect_to root_path
-    else
-      flash[:notice] = "Couldn't Join Group"
-      redirect_to root_path
-    end
+    p = user_group_params
+    UserGroup.create(:comment => p["comment"], :user_id => p['user_id'], :group_id => p["group_id"])
+    redirect_to group_path(p["group_id"])
   end
 
   private
-
+  def user_group_params
+    params.permit('group_id', "user_id", "comment")
+  end
 end

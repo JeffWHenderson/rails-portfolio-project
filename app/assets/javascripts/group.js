647a9f2b24f478ed85b72f64a8a755090f1bc265
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-01-23 13:56:29 -0500

    added a javascript function to access the endpoint of meetups on the show-meetups click.

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
 
   $('.show-meetups').on('click', (e) =>{
     e.preventDefault()
-    console.log("HIJACKED!!!!")
+    let id = $(this).attr('data-id')
+    fetch(`/groups/1/meetups`)
+    .then(res => res.json())
+    .then(meetups => {
+      //$('.show-meetups').append(meetups)
+      console.log(meetups)
+    })
   })
 })
